23790bacf506c63992d768f144796d95de2cbd64
Marwa El-Shnawii <marwaelshnawii@gmail.com>
2024-12-13 15:14:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function popup1(){
+    window.alert("Hello in our site");
+}
+function popup2(){
+    window.prompt(" Enter your Name");
+}
+function popup3(){
+    window.alert("we wish you a unique and enjoyable experience");
+}
+function popup4(){
+    window.alert("the restaurant was established in 1979 and there is free delivery servise");
+}
